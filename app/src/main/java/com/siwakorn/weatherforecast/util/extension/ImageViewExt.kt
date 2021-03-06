1c1f7c56e0bda828c0ab69ed32930ce56c0975fa
siwakorn <siwakorn.m.0595@gmailcom>
2021-03-06 10:41:17 +0700

    add some weather response

--- app/src/main/java/com/siwakorn/weatherforecast/util/extension/ImageViewExt.kt
+++ app/src/main/java/com/siwakorn/weatherforecast/util/extension/ImageViewExt.kt
@@ -2,9 +2,11 @@ package com.siwakorn.weatherforecast.util.extension
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.siwakorn.weatherforcast.R
 
 fun ImageView.loadImageUrl(url: String?) {
     Glide.with(this.context)
         .load(url)
+        .error(R.drawable.ic_error)
         .into(this)
 }
